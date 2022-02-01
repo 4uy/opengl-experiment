49888631a5e13fe9d093959d1c50aebad35b20fb
4uy <64391250+4uy@users.noreply.github.com>
2022-02-01 11:46:34 -0600

    Add "helpful" comments, might remove some later

--- src/main.cc
+++ src/main.cc
@@ -9,19 +9,26 @@
 int main() {
     std::cout << "Hello, world!" << std::endl;
 
+    // Initialize GLFW & create a window
     Window window(1280, 720, "Minecraft");
     GLFWwindow* native_window = window.Get();
 
+    // Start tracking some time stats
     Time time;
 
+    // Pre-loop OpenGL stuff
     glViewport(0, 0, window.GetWidth(), window.GetHeight());
     glClearColor(0.1f, 0.1f, 0.2f, 0.0f);
 
+    // Game loop
     while (!glfwWindowShouldClose(native_window)) {
+        // Update time
         time.Update();
 
+        // OpenGL stuff
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Update window
         window.Update();
     }
 
